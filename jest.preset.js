aa92d9a9bed31ed92284a2345d8a3341cad0c139
Denis Martinho <denis.fmartinho@gmail.com>
2021-12-08 10:05:15 +0100

    upgrade to 13

--- jest.preset.js
+++ jest.preset.js
@@ -2,10 +2,22 @@ const nxPreset = require('@nrwl/jest/preset');
 module.exports = {
   ...nxPreset,
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
+  // transform: {
+  //   '^.+\\.(ts|js|html)$': 'ts-jest',
+  // },
+  globals: {
+    'ts-jest': {
+      stringifyContentPathRegex: '\\.(html|svg)$',
+
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
   },
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
+  transform: {
+    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
 };
